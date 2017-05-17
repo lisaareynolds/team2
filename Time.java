434c1de3d6be502c7e92eb68a3fb4618bdc6922f
Wolfleaf <bluepandaleaf@gmail.com>
2017-05-17 09:08:59 -0600

    Update Time.java

--- Time.java
+++ Time.java
@@ -4,17 +4,18 @@ This class pauses the program temporarly for the given time values:
 To wait x seconds:    EasyTime.Wait(x) 
 
 Examples:
+
 To wait 2 seconds:    EasyTime.Wait(2);
 To wait 3.5 seconds:  EasyTime.Wait(3.5);
 
 This was created for pauses when needed in "Pork"
-*//
+*/
 
 public class Time
 {
-    public static void Wait(double waitTime)
+    public static void Wait(int waitTime)
     {
-        int waitTime = waitTime * 1000; //1000 ticks is a second
+        waitTime = waitTime * 1000; //1000 ticks is a second
         try
         {
             Thread.sleep(waitTime);
