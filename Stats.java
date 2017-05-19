54f26627d73d47e5b1024b38ba53f6b28b2782fc
Wolfleaf <bluepandaleaf@gmail.com>
2017-05-19 08:31:17 -0600

    Create Stats.java

---
+++ Stats.java
@@ -0,0 +1,8 @@
+public class Stats
+{
+    public static void Stats(int health, String item)
+    {
+        System.out.println("Health: " + health + "/5");
+        System.out.println("You are currently holding " + item);
+    }
+}
