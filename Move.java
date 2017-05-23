a8405d8d49b9de861e5af3a2bbfe7cbb3117dd49
Wolfleaf <bluepandaleaf@gmail.com>
2017-05-23 08:43:16 -0600

    Update Move.java

--- Move.java
+++ Move.java
@@ -1,1 +1,74 @@
-//nothing right now
+import java.util.Scanner;
+
+/**
+ * Created by atho9080 on 5/23/2017.
+ */
+public class Move
+{
+    public static int Move(int currentx, int currenty)
+    {
+        Scanner in = new Scanner(System.in);
+        boolean errorCheck = false;
+
+        int[][] location = new int[10][10];//{1,2,3,4,5,6}}; //FOR THIS, PUT 0 IN ALL THE PLACES YOU CANNOT GO TOO
+
+        String input = in.nextLine();
+        if (input.toLowerCase().equals("north")) //might want to make this a switch statement later
+        {
+            if (location[currentx][currenty + 1] == 0)
+            {
+                System.out.println("You cannot go that way");
+                return currenty; //returns the same value, as you did not move, so that you exit out of the method
+            }
+            else
+            {
+                currenty = currenty+1;
+                return currenty;
+            }
+        }
+        else if (input.toLowerCase().equals("south"))
+        {
+            if (location[currentx][currenty - 1] == 0)
+            {
+                System.out.println("You cannot go that way");
+                return currenty;
+            }
+            else
+            {
+                currenty = currenty-1;
+                return currenty;
+            }
+        }
+        else if (input.toLowerCase().equals("east"))
+        {
+            if (location[currentx - 1][currenty] == 0)
+            {
+                System.out.println("You cannot go that way");
+                return currenty;
+            }
+            else
+            {
+                currentx = currentx - 1;
+                return currentx;
+            }
+        }
+        else if (input.toLowerCase().equals("west"))
+        {
+            if (location[currentx + 1][currenty] == 0)
+            {
+                System.out.println("You cannot go that way");
+                return currenty;
+            }
+            else
+            {
+                currentx = currentx + 1;
+                return currentx;
+            }
+        }
+        else
+        {
+            System.out.println("Not a correct direction");
+            return currenty;
+        }
+    }
+}
