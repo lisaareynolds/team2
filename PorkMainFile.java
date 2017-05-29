ba2c6593ce5ca1caceb4c8c48b44ea0997b8623e
Wolfleaf <bluepandaleaf@gmail.com>
2017-05-29 11:24:16 -0600

    Update PorkMainFile.java

--- PorkMainFile.java
+++ PorkMainFile.java
@@ -6,6 +6,8 @@ public class PorkMainFile
     {
         Scanner in = new Scanner(System.in);
         boolean finished = false;
+        int time = 15;
+        boolean door = false;
 
         System.out.println("Version 0.1, all rights reserved");
         System.out.println("  ____     ___    ____    _  __");
@@ -26,86 +28,132 @@ public class PorkMainFile
         String item = "nothing";
         int health = 5;
 
-        /**
-        anArray[0,0] = "There lies a metal table with a flickering LED screen. Between each flash you can make out these words: /n SECUTIRY QUESTION- It is greater than God and more evil than the devil. The poor have it, the rich need it, and if you eat it you’ll die. What is it? As you looked back it seemed Abligale was deep in thought, determind to figure it out";
-        anArray[1,0] = "The final door, You can see your reflection in its metlaic shine. A glipse of hope, one last chance. Now if only you had the code.";
-        anArray[2,0] = "In the corner of the room there's a tablet on the wall, it has a keyboard poped out. The screen is cracked, but you can make out that it needs a six letter passcode. This must be where Dr. Erskine told you to put the code. you can feel the blood pumping from your heart, desprate to get out of this hell.";
-        anArray[3,0] = 0;
-        anArray[4,0] = " ";
-        anArray[5,0] = 600;
-        anArray[6,0] = 700;
-        anArray[7,0] = 800;
-        anArray[8,0] = 900;
-        anArray[9,0] = 1000;
-         **/
-
-        while (finished == false)
-        {
+        while (finished == false) {
             String input = in.nextLine();
-            
+            input = input.toLowerCase();
+            boolean action = true;
 
-            if (input.toLowerCase().equals("credits")) //first couple classes don't use any other classes
+            switch (input)
             {
-                System.out.println("By Alex Thomsen, Luke Mendes, Andy Hsu, That Bryson Guy");
+                case "credits":
+                    System.out.println("By Alex Thomsen, Luke Mendes, Andy Hsu, That Bryson Guy");
+                    action = false;
+                    break;
+                case "help":
+                    System.out.println("Command List: credits, help, inspect, map, move, stats, suicide, surroundings, quit");
+                    action = false;
+                    break;
+                case "surroundings":
+                    System.out.println(surroundings);
+                    action = false;
+                    break;
+                case "map":
+                    Map.Map(currentx, currenty);
+                    action = false;
+                    break;
+                case "move":
+                    System.out.println("Which direction? ");
+                    String direction = in.nextLine();
+                    if (direction.toLowerCase().equals("west") || direction.toLowerCase().equals("w")) {
+                        currentx = Move.X(currentx - 1, currenty, false);
+                    } else if (direction.toLowerCase().equals("east") || direction.toLowerCase().equals("e")) {
+                        currentx = Move.X(currentx + 1, currenty, true);
+                    } else if (direction.toLowerCase().equals("north") || direction.toLowerCase().equals("n")) {
+                        currenty = Move.Y(currentx, currenty - 1, true, door);
+                    } else if (direction.toLowerCase().equals("south") || direction.toLowerCase().equals("s")) {
+                        currenty = Move.Y(currentx, currenty + 1, false, door);
+                        if (currenty == 100)
+                        {
+                            System.out.println("More content will be added later (maybe...) (probably not...)");
+                            finished = true;
+                            break;
+                        }
+                    } else {
+                        System.out.println("Not a correct direction");
+                        action = false;
+                    }
+                    System.out.println("Current x value: " + currentx);
+                    System.out.println("Current y value: " + currenty);
+                    break;
+                case "stats":
+                    Stats.Stats(health, item);
+                    action = false;
+                    break;
+                case "inspect":
+                    if (currentx == 1)
+                    {
+                        boolean type = Inspect.Inspect(currentx, currenty, health);
+                        if (type == true)
+                        {
+                            if (health == 5)
+                            {
+                                System.out.println("You already have full health!");
+                            }
+                            else
+                            {
+                                System.out.println("You used the first aid kit (+1 health)");
+                                health = health + 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        door = Inspect.Inspect(currentx, currenty, health);
+                    }
+                    break;
+                case "suicide":
+                    Death.Death("Edgy");
+                    finished = true;
+                    break;
+                case "quit":
+                    finished = true;
+                    System.out.println("Coward");
+                    break;
+                default:
+                    System.out.println("That is not a command, type \"help\" for list of commands");
+                    action = false;
+                    break;
             }
-            else if (input.toLowerCase().equals("help"))
+            if (action == true)
             {
-                System.out.println("Command List: credits, help, inspect, map, move, stats, suicide");
+                time = time - 1;
             }
-            else if (input.toLowerCase().equals("surroundings"))
+            switch (time)
             {
-                System.out.println(surroundings);
+                case 5:
+                    System.out.println("Suddenly, the vents above turn on, and a greenish gas starts spilling out.");
+                    break;
+                case 4:
+                    System.out.println("You begin to choke on the gas (-1 health)");
+                    health = health - 1;
+                    break;
+                case 3:
+                    System.out.println("The gas rises.  Yours eyes begin to burn (-1 health)");
+                    health = health - 1;
+                    break;
+                case 2:
+                    System.out.println("You start coughing up blood (-1 health)");
+                    health = health - 1;
+                    break;
+                case 1:
+                    System.out.println("Your skin is cracking and you can barely see (-1 health)");
+                    health = health - 1;
+                    break;
+                case 0:
+                    System.out.println("You can barely see through the blood that now covers your eyes (-1 health)");
+                    health = health - 1;
+                    break;
+                case -1:
+                    System.out.println("Suddenly, the gas is sucked out of the room.  You barely survived");
+                    time = 100;
+                    break;
             }
-            else if (input.toLowerCase().equals("map")) //after this classes uses other classes
+            if (health == 0)
             {
-                Map.Map(currentx, currenty); //calls the class to run the action (CLASS NOT MADE YET)
-            }
-            else if (input.toLowerCase().equals("move"))
-            {
-                System.out.println("Which direction? ");
-                String direction = in.nextLine();
-                if (direction.toLowerCase().equals("west"))
-                {
-                    currentx = Move.X(currentx - 1,currenty, false);
-                }
-                else if (direction.toLowerCase().equals("east"))
-                {
-                    currentx = Move.X(currentx + 1,currenty, true);
-                }
-                else if (direction.toLowerCase().equals("north"))
-                {
-                    currenty = Move.Y(currentx,currenty - 1, true);
-                }
-                else if (direction.toLowerCase().equals("south"))
-                {
-                    currenty = Move.Y(currentx,currenty + 1, false);
-                }
-                else
-                {
-                    System.out.println("Not a correct direction");
-                }
-                System.out.println("Current x value: " + currentx);
-                System.out.println("Current y value: " + currenty);
-            }
-            else if (input.toLowerCase().equals("stats"))
-            {
-                Stats.Stats(health, item);
-            }
-            else if (input.toLowerCase().equals("inspect"))
-            {
-                //inspect(location); //can't finish because there is no layout yet
-            }
-            else if (input.toLowerCase().equals("suicide"))
-            {
-                Death.Death("Edgy");
-                break;
-            }
-            else
-            {
-                System.out.println("That is not a command, type \"help\" for list of commands");
+                Death.Death("Well that didn't work out");
+                finished = true;
             }
         }
-
         System.out.println("Thank you for playing");
     }
 }
