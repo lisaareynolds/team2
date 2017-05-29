4674f3d26ec4c6cc5f94eab309c6b6f33b82f0c2
Wolfleaf <bluepandaleaf@gmail.com>
2017-05-29 11:25:44 -0600

    Update Death.java

--- Death.java
+++ Death.java
@@ -1,27 +1,8 @@
-import java.util.Random;
-
 public class Death
 {
-    public static void Death()
+    public static void Death(String message)
     {
-        Random rand = new Random();
-        int saying = rand.nextInt(5) + 1;
-        
-        switch (saying)
-        {
-            case 1: System.out.println("Next time, use your head");
-                    break;
-            case 2: System.out.println("Don't be an idiot, you idiot");
-                    break;
-            case 3: System.out.println("Don't be sad, lot's of people fail at life");
-                    break;
-            case 4: System.out.println("There are no words");
-                    break;
-            case 5: System.out.println("Usually, mistakes are good as you learn from them...");
-                    break;
-            default: System.out.println("Class DEATH is producing errors");
-                    break;
-        }
+        System.out.println(message);
         System.out.println("You died");
     }
 }
